f9feb22d4302dbdc1a7e22b4b916a49d8c6fdd53
ParasiiAlex <48950010+ParasiiAlex@users.noreply.github.com>
2020-10-21 20:52:41 +0300

    Add files via upload

---
+++ Common/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public class Config
+    {
+        public const string BrokerAddress = "http://127.0.0.1:5050";
+        public const string SubscriberAddres = "http://127.0.0.1:0";
+
+        public const int TIME_TO_WAIT_FOR_WORKER = 2000;
+    }
+}
